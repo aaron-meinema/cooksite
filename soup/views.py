7e334d3dd3ea6deb34ce5a005c6277624411226d
Aaron <aaronwillem@hotmail.com>
2018-04-17 21:54:32 +0200

    ready to start webscraping

--- soup/views.py
+++ soup/views.py
@@ -1,20 +1,32 @@
 from django.shortcuts import get_object_or_404, render
 from .models import Site, Page, Content
 
+page_list = Page.objects.all()
+context ={
+    'page_list': page_list
+}
+
 
 def index(request):
-    page_list = Page.objects.all()
-    context = {
-        'page_list': page_list,
-    }
-    title = "super awesome"
-    return render(request, 'soup/index.html', context, title)
+    context['title'] = 'this is the index page'
+    return render(request, 'soup/index.html', context)
 
 
 def add(request):
-    return render(request, 'soup/index.html')
+    site_list = Site.objects.all()
+    context['title'] ='add page here'
+    context['site_list'] = site_list
+
+    try:
+        selected_page = request.POST['page']
+    except KeyError:
+        return render(request, 'soup/add.html', context)
+    else:
+        context['selected_page'] = selected_page
+        return render(request, 'soup/add.html', context)
 
 
 def page(request, page_id):
     rendered_page = get_object_or_404(Page, id=page_id)
-    return render(request, 'soup/index.html', {'rendered_page': rendered_page})
+    context['rendered_page'] = rendered_page
+    return render(request, 'soup/index.html', context,)
