fabb3152ea116cebdde95811dd895d6f2ed302d0
Aaron <aaronwillem@hotmail.com>
2018-05-14 12:12:14 +0200

    Setup .py added

--- soup/views.py
+++ soup/views.py
@@ -59,5 +59,5 @@ def page(request, page_id):
     context['display_content'] = display_content
     context['rendered_page'] = rendered_page
     context['active'] = page_id
-    context['title'] = Page.objects.get(id=page_id).name.__str__() 
+    context['title'] = Page.objects.get(id=page_id).name.__str__()
     return render(request, 'soup/pages.html', context,)
